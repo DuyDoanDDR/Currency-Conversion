82c3d6f930925230c57d01f8df8c700360ccba3e
Doan Minh Duy <duydoan812@gmail.com>
2025-03-11 23:28:07 +0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int Rate = 25000;
+int VND;
+int USD;
+Console.WriteLine("So tien USD can quy doi : ");
+USD = int.Parse(Console.ReadLine());
+VND = USD * Rate;
+Console.WriteLine("So tien VND tuong ung : " + VND + " dong ");
